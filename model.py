0f7a9c03c978d3a7871a5f45a331e2cd904f1071
batheand <batheand@gmail.com>
2023-09-16 11:41:31 +0300

    now saves the trained model

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 #The model will use the MNIST dataset, contain my own CNN
 #http://yann.lecun.com/exdb/mnist/
 
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import os
 
 class Model:
     def __init__(self):
@@ -42,11 +44,12 @@ class Model:
         self.ds_test = self.ds_test.prefetch(tf.data.AUTOTUNE)
 
     def _initialize_models(self):
-        tfds_model = tf.keras.models.Sequential([
+        tf_docs_model = tf.keras.models.Sequential([
             tf.keras.layers.Flatten(input_shape=(28, 28)),
             tf.keras.layers.Dense(128, activation='relu'),
             tf.keras.layers.Dense(10)
         ])
+        #print("tf_docs")
         small_model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
             tf.keras.layers.MaxPooling2D((2, 2)),
@@ -54,6 +57,7 @@ class Model:
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
+        #print("small")
         medium_model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
             tf.keras.layers.MaxPooling2D((2, 2)),
@@ -65,6 +69,7 @@ class Model:
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
+        #print("medium")
         large_model = tf.keras.Sequential([
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
@@ -80,9 +85,9 @@ class Model:
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(10, activation='softmax')
         ])
-
+        #print("large")
         self.models = {
-        "tfds": tfds_model,
+        "tf_docs": tf_docs_model,
         "small": small_model,
         "medium": medium_model,
         "large": large_model
@@ -102,11 +107,26 @@ class Model:
             epochs=6,
             validation_data=self.ds_test,
         )
+
+        print("Model Trained!")
+
+        self.models[model_name].summary()
+
+        path = os.path.join(os.getcwd(), "out")
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        path = os.path.join(path,  model_name+".keras")
+
+        self.models[model_name].save(path)
+
+    
     
 
 def main():
     model = Model()
-    model.custom_cnn()
+    model.custom_cnn("medium")
 
 
 if __name__ == "__main__":
